c80f609d23758f4d643697930544bf51447a5727
Chloe Kurdziel <chloekurdziel@gmail.com>
2022-10-24 18:34:37 -0400

    edit and update routescomplete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,11 +6,11 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
-breads.get('/:arrayIndex', (req,res) => {
-    if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-        bread: Bread[req.params.arrayIndex],
-        index: req.params.arrayIndex,
+breads.get('/:indexArray/edit', (req,res) => {
+    if (Bread[req.params.indexArray]) {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray,
         title: 'Index Page'
     })
 } else {
@@ -18,7 +18,7 @@ breads.get('/:arrayIndex', (req,res) => {
 }
 })
 
-breads.post('/', (req, res) => {
+breads.put('/', (req, res) => {
     if (!req.body.image) {
         req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
     }
@@ -27,8 +27,8 @@ breads.post('/', (req, res) => {
     } else {
         req.body.hasGluten = 'false'
     }
-    Bread.push(req.body)
-    res.redirect('/breads')
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 breads.delete('/:indexArray', (req, res) => {
