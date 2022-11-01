da5e5cd52144ff4a713c274de64a09e39b4bbb11
Chloe Kurdziel <chloekurdziel@gmail.com>
2022-10-31 21:15:11 -0400

    added bakers

--- views/new.jsx
+++ views/new.jsx
@@ -28,6 +28,15 @@ function New () {
                 <br />
                 <input type="submit"/>
             </form>
+            <label htmlFor="baker">Baker</label>
+            <select name="baker" id="baker">
+                <option value="Rachel">Rachel</option>
+                <option value="Monica">Monica</option>
+                <option value="Joey">Joey</option>
+                <option value="Chandler">Chandler</option>
+                <option value="Ross">Ross</option>
+                <option value="Phoebe">Phoebe</option>
+            </select>
         </Default>
     )
 }
